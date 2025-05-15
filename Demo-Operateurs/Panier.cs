52c4ac522947a725b5f4eeabd49e63c5af9996bd
Samuel Legrain <samuel.legrain@bstorm.be>
2025-05-15 16:38:37 +0200

    Ajout Démo-Opérateurs

---
+++ Demo-Operateurs/Panier.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Operateurs
+{
+    public class Panier
+    {
+        private List<Fruit> _fruits = new List<Fruit>();
+
+        public void AddFruit(Fruit fruit)
+        {
+            _fruits.Add(fruit);
+        }
+
+        public static Panier operator +(Panier left, Panier right)
+        {
+            Panier result = new Panier();
+
+            foreach (Fruit f in left._fruits)
+            {
+                result.AddFruit(f);
+            }
+
+            foreach (Fruit f in right._fruits)
+            {
+                result.AddFruit(f);
+            }
+
+            return result;
+        }
+
+        public static int operator +(Panier left, Fruit right)
+        {
+            left.AddFruit(right);
+
+            return left._fruits.Count;
+        }
+
+        public static Panier[] operator *(Panier left, int right)
+        {
+            Panier[] result = new Panier[right];
+            for(int i = 0; i < result.Length; i++)
+            {
+                result[i] = new Panier();
+                foreach (Fruit f in left._fruits)
+                {
+                    result[i].AddFruit(f);
+                }
+            }
+
+            return result;
+        }
+
+        public static bool operator < (Panier left, Panier right)
+        {
+            return left._fruits.Count < right._fruits.Count;
+        }
+
+        public static bool operator > (Panier left, Panier right)
+        {
+            return left._fruits.Count > right._fruits.Count;
+        }
+    }
+}
---
+++ Demo-Operateurs/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Demo_Operateurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Rappel opérateur system
+             * int nb1 = 5, nb2 = 7, result;
+            result = nb1 + nb2;
+
+            Console.WriteLine($"{nb1} + {nb2} = {result}");
+
+            string word1 = "Hello", word2 = "world";
+
+            Console.WriteLine(word1 + " " + word2 + "!");
+            */
+
+            Panier p1 = new Panier();
+            p1.AddFruit(new Fruit());
+            p1.AddFruit(new Fruit());
+            p1.AddFruit(new Fruit());
+
+            //Console.WriteLine($"Dans le panier 1 nous avons : {p1.fruits.Count} fruits");
+
+            Panier p2 = new Panier();
+            p2.AddFruit(new Fruit());
+            p2.AddFruit(new Fruit());
+
+            //Console.WriteLine($"Dans le panier 2 nous avons : {p2.fruits.Count} fruits");
+
+            Panier total = p1 + p2;
+
+            //Console.WriteLine($"Si je réunis le tout, j'obtiens un panier de {total.fruits.Count} fruits");
+
+            Console.WriteLine($"Ajoutons-y un fruit! Cela me donne : {total + new Fruit()} fruits.");
+
+            Panier voisin = new Panier();
+            voisin.AddFruit(new Fruit());
+            voisin.AddFruit(new Fruit());
+            voisin.AddFruit(new Fruit());
+            voisin.AddFruit(new Fruit());
+            voisin.AddFruit(new Fruit());
+
+            if (total > voisin)
+            {
+                Console.WriteLine("J'ai plus de fruit que le voisin!");
+            }
+            else if(total < voisin)
+            {
+                Console.WriteLine("Le voisin est encore venu voler mes fruits!");
+            }
+            else
+            {
+                Console.WriteLine("Il en a autant que moi, mais les miens sont plus beaux!");
+            }
+                    
+        }
+    }
+}
